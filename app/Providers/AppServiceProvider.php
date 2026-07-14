3306b4ec3442ad6dfea53048e04e4909a579efff
Mario Lasić <mario.lasic@outlook.com>
2026-07-14 18:59:42 +0200

    dev okolina

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(app()->environment('production'))
+        if(app()->environment('production') || app()->environment('dev'))
         {
             URL::forceScheme('https');
         }
